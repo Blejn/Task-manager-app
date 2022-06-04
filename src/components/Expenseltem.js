4da24bcdfe99946285a61beb6aa93d296128f76d
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2022-06-04 19:12:26 +0200

    Section one - Working with components

---
+++ src/components/Expenseltem.js
@@ -0,0 +1,23 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from'./Card';
+
+function ExpenseItem(props){
+
+
+    return (
+       
+   <Card className='expense-item'>
+     <ExpenseDate date={props.date}/>
+       <div className='expense-item_description'>
+           <h2>
+            {props.title}
+           </h2>
+           <div className='expense-item_price'>{props.amount}</div>
+       </div>
+   </Card>
+    );
+        
+ 
+}
+export default ExpenseItem
