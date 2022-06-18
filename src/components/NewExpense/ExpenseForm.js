e9d62b05e8f9d30bacd9fc9f107c36bbb62ee897
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2022-06-18 18:19:04 +0200

    Adding expsense to List in console.log

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-undef */
 import React,{useState} from "react";
 import './ExpenseForm.css';
 
-const ExpenseForm=()=>{
+const ExpenseForm=(props)=>{
 // const [enteredTitle,setEnteredTitle]= useState('');
 // const[enteredAmount,setEnteredAmount]=useState('');
-// const[enteredDate,setEnteredDate]=useState('');
-const[userInput,setUserInput]= useState({
-enteredTitle:'',
-enteredAmount:'',
-enteredDate:''
+//  const[enteredDate,setEnteredDate]=useState('');
+ const[userInput,setUserInput]= useState({
+ enteredTitle:'',
+ enteredAmount:'',
+ enteredDate:''
 
-});
+ });
     const titleChangeHandler=(event)=>{
         // setUserInput({
         //     ...userInput,
@@ -51,21 +52,22 @@ const submitHandler=(event)=>{
         amount:userInput.enteredAmount,
         date:new Date(userInput.enteredDate)
     };
-    console.log(expenseData);
+     props.onSaveExpenseDatas(expenseData);
+    setUserInput('')
 };
     return(
      <form onSubmit={submitHandler} className="new-expense__controls">
    <div className="new-expense__control">
     <label></label>
-    <input type='text' onChange={titleChangeHandler} placeholder="title"/>
+    <input type='text' value={userInput.enteredTitle} onChange={titleChangeHandler} placeholder="title"/>
    </div>
    <div className="new-expense__control">
     <label></label>
-    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+    <input type='number' min="0.01"  step="0.01"  value={userInput.enteredAmount} onChange={amountChangeHandler}/>
    </div>
    <div className="new-expense__control">
     <label></label>
-    <input type='date' min="2022-06-16" max="2023-01-01" onChange={dateChangeHandler}/>
+    <input type='date' min="2022-06-16" max="2023-01-01"  value={userInput.enteredDate} onChange={dateChangeHandler}/>
    </div>
    <div className="new-expense__actions">
     <button type="submit" >Add Expense</button>
